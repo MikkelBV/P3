495f56bef8342854cb33d16dc5b88bc1ac52bb03
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2017-10-01 15:12:21 +0200

    simple feature detection on video (runs slowly)

--- src/P3/main.cpp
+++ src/P3/main.cpp
@@ -1,29 +1,73 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include <vector>
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char* argv[]) {
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
+// wrappers
+Mat makeHistogram(Mat img) {
+	//Make histograms
+	int histSize = 256;
 
-	Mat edges;
-	namedWindow("video_t", 1);
-	namedWindow("video", 1);
+	//Ranges
+	float range[] = { 0, 256 };
+	const float* histRange = { range };
+
+	Mat histogram;
+	calcHist(&img, 1, 0, Mat(), histogram, 1, &histSize, &histRange, true, false);
+
+	int hist_w = 600, hist_h = 400;
+	int bin_w = cvRound((double)hist_w / histSize);
+	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+	normalize(histogram, histogram, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+
+	/// Draw for each channel
+	for (int i = 1; i < histSize; i++) {
+		line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(histogram.at<float>(i - 1))),
+			Point(bin_w*(i), hist_h - cvRound(histogram.at<float>(i))),
+			Scalar(255, 0, 0), 2, 8, 0);
+	}
+
+	return histImage;
+}
+void convertToGreyscale(Mat *img) {
+	cvtColor(*img, *img, COLOR_BGRA2GRAY);
+}
+void convertToGreyscale(Mat *img, Mat *target) {
+	cvtColor(*img, *target, COLOR_BGRA2GRAY);
+}
+
+int main(int argc, char* argv[]) {
+	VideoCapture video("test.mp4");
+	//VideoCapture video(0);
+	namedWindow("Simon");
 
 	while (true) {
-		Mat frame, tframe, hsv;
-		cap >> frame; 
-		cvtColor(frame, hsv, COLOR_BGR2HSV);
-		Scalar lower_red_l = Scalar(0, 0, 0);
-		Scalar lower_red_h = Scalar(150, 150, 150);
-		inRange(hsv, lower_red_l, lower_red_h, tframe);
-		imshow("video_t", tframe);
-		imshow("video", frame);
-		waitKey(50);
+		Mat frame;
+		video >> frame;
+		convertToGreyscale(&frame);
+
+		std::vector< cv::Point2f > corners;
+
+		int maxCorners = 100;
+		double qualityLevel = 0.01;
+		double minDistance = 10.;
+
+		goodFeaturesToTrack(frame, corners, maxCorners, qualityLevel, minDistance);
+
+		for (size_t i = 0; i < corners.size(); i++) {
+			cv::circle(frame, corners[i], 6, cv::Scalar(255.), 2);
+		}
+
+		imshow("Simon", frame);
+
+		char key = waitKey(1);
+		if (key > 0)
+			break;
 	}
+
 	return 0;
 }
